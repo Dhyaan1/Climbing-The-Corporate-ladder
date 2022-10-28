6b0ce630ae686652b6938e691955347dabc202fb
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2022-10-28 23:25:11 +0530

    Finish this

--- Level-2/pscript.js
+++ Level-2/pscript.js
@@ -1,7 +1,23 @@
+let Question1_answered = false
+let Question2_answered = false
+let Question3_answered = false
+let Question4_answered = false
+let Question5_answered = false
+let correctAnswer1 = false // turn this to true when correct answer and ket there be magic
+let correctAnswer2 = false
+let correctAnswer3 = false
+let correctAnswer4 = false
+let correctAnswer5 = false
+
 const g1 = document.getElementById("guy1")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
+    if(offset>= 1500 && Question1_answered==false){
+        this.alert("Hello1")   // turn this into a QnA
+        Question1_answered = true // use an if statment and change boolean correctAnswer1 if the answer is coorect
+        checkanswer1();  
+    }
     g1.style.backgroundPositionY = offset * 0.905 + "px"
 })
 
@@ -9,6 +25,11 @@ const g2 = document.getElementById("guy2")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
+    if(offset>= 2500 && Question2_answered==false){
+        this.alert("Hello2")
+        Question2_answered = true
+        checkanswer2();
+    }
     g2.style.backgroundPositionY = offset * 0.92 + "px"
 })
 
@@ -16,6 +37,11 @@ const g3 = document.getElementById("guy3")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
+    if(offset>= 3500 && Question3_answered==false){
+        this.alert("Hello3")
+        Question3_answered = true
+        checkanswer3();
+    }
     g3.style.backgroundPositionY = offset * 0.93 + "px"
 })
 
@@ -23,6 +49,11 @@ const g4 = document.getElementById("guy4")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
+    if(offset>=4500 && Question4_answered==false){
+        this.alert("Hello4")
+        Question4_answered = true
+        checkanswer4();
+    }
     g4.style.backgroundPositionY = offset * 0.92 + "px"
 })
 
@@ -30,5 +61,75 @@ const g5 = document.getElementById("guy5")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
+    if(offset>=5500 && Question5_answered==false){
+        this.alert("Hello5")
+        Question5_answered = true
+        checkanswer5();
+    }
     g5.style.backgroundPositionY = offset * 0.905 + "px"
 })
+
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+
+const player = {
+    score: 0,
+};
+
+function checkanswer1(){
+    if (correctAnswer1) {
+        player.score = player.score + 1000;
+        updateScore();
+    }
+    else{
+        player.score = player.score - 2000;
+        updateScore();
+    }
+}
+
+function checkanswer2(){
+    if (correctAnswer2) {
+        player.score = player.score + 1000;
+        updateScore();
+    }
+    else{
+        player.score = player.score - 2000;
+        updateScore();
+    }
+}
+
+function checkanswer3(){
+    if (correctAnswer3) {
+        player.score = player.score + 1000;
+        updateScore();
+    }
+    else{
+        player.score = player.score - 2000;
+        updateScore();
+    }
+}
+
+function checkanswer4(){
+    if (correctAnswer4) {
+        player.score = player.score + 1000;
+        updateScore();
+    }
+    else{
+        player.score = player.score - 2000;
+        updateScore();
+    }
+}
+
+function checkanswer5(){
+    if (correctAnswer5) {
+        player.score = player.score + 1000;
+        updateScore();
+    }
+    else{
+        player.score = player.score - 2000;
+        updateScore();
+    }
+}
+
+updateScore();
