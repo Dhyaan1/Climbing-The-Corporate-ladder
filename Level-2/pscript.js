1c3374502c34b76b98a7b5725291c81d3c0de908
fringewidth <hrishiksaibm@gmail.com>
2022-10-28 17:14:29 +0530

    pushing everything

--- Level-2/pscript.js
+++ Level-2/pscript.js
@@ -4,3 +4,17 @@ window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
     d5.style.backgroundPositionX = offset * -2.7 + "px"
 })
+
+const g1 = document.getElementById("guy1")
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset
+    g1.style.backgroundPositionY = offset * -2.7 + "px"
+})
+
+const g2 = document.getElementById("guy2")
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset
+    g2.style.backgroundPositionY = offset * -2.7 + "px"
+})
