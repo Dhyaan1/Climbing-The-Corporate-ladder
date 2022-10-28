9e82054f01dfaea5fc88cdadcbe00971fcfb0820
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2022-10-28 20:35:38 +0530

    yyyy

--- Level-2/pscript.js
+++ Level-2/pscript.js
@@ -2,33 +2,33 @@ const g1 = document.getElementById("guy1")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
-    g1.style.backgroundPositionY = offset * -2 + "px"
+    g1.style.backgroundPositionY = offset * 0.905 + "px"
 })
 
 const g2 = document.getElementById("guy2")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
-    g2.style.backgroundPositionY = offset * -1.5 + "px"
+    g2.style.backgroundPositionY = offset * 0.92 + "px"
 })
 
 const g3 = document.getElementById("guy3")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
-    g3.style.backgroundPositionY = offset * -0.3 + "px"
+    g3.style.backgroundPositionY = offset * 0.93 + "px"
 })
 
 const g4 = document.getElementById("guy4")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
-    g4.style.backgroundPositionY = offset * -0.6 + "px"
+    g4.style.backgroundPositionY = offset * 0.92 + "px"
 })
 
 const g5 = document.getElementById("guy5")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
-    g5.style.backgroundPositionY = offset * -1 + "px"
+    g5.style.backgroundPositionY = offset * 0.905 + "px"
 })
