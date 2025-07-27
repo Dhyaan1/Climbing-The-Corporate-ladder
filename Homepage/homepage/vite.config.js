3120998d7e5ea9419660a32af9f5628135c490d6
hrishik <hrishiksai@outlook.com>
2025-07-27 15:26:06 +0530

    add a deploy

---
+++ Homepage/homepage/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Climbing-The-Corporate-ladder/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+}) 
